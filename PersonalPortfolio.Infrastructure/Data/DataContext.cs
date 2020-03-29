75ab1bf6ccfb5343a017043dbddeb939b2faae9a
Khalid ESSAADANI <khalid_essaadani@hotmail.fr>
2020-03-29 16:13:53 +0100

    Created DataContext.

---
+++ PersonalPortfolio.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalPortfolio.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalPortfolio.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Owner> Owner { get; set; }
+    }
+}
