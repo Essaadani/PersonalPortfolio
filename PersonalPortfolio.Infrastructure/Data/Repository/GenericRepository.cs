32bdbb8c1c6944d865174de0084db232c4e6a996
Khalid ESSAADANI <khalid_essaadani@hotmail.fr>
2020-03-29 16:27:32 +0100

    Implemeneted GenericRepository.

---
+++ PersonalPortfolio.Infrastructure/Data/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonalPortfolio.Infrastructure.Data.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DataContext _context;
+
+        private DbSet<T> table = null;
+
+        public GenericRepository(DataContext context)
+        {
+            _context = context;
+            table = _context.Set<T>();
+        }
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+           table.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            table.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ PersonalPortfolio.Infrastructure/Data/Repository/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalPortfolio.Infrastructure.Data.Repository
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
+    }
+}
